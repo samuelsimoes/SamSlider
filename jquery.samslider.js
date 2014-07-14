fe190c0208220c2109b04f26af607aeaecf485b2
Samuel Simões <slip.cm@gmail.com>
2014-07-14 00:25:48 -0300

    Refactored the slider code

--- jquery.samslider.js
+++ jquery.samslider.js
@@ -79,7 +79,7 @@ SamSlider.Controller.prototype = {
 
 (function($){
   $.fn.samSlider = function(options) {
-    var config = $.extend( {
+    options = $.extend({
       previousButtonSelector: ".previous-btn",
       nextButtonSelector: ".next-btn",
       auto: true,
@@ -92,111 +92,102 @@ SamSlider.Controller.prototype = {
     }, options);
 
     var $container = $(this),
-        $nextButton = $container.find(config.nextButtonSelector),
-        $previousButton = $container.find(config.previousButtonSelector),
-        $paginationContainer = $container.find(config.paginationContainerSelector),
-        $slidesContainer = $container.find(config.slidesContainerSelector),
-        $slides = $slidesContainer.children(config.slideSelector);
+        $nextButton = $container.find(options.nextButtonSelector),
+        $previousButton = $container.find(options.previousButtonSelector),
+        $paginationContainer = $container.find(options.paginationContainerSelector),
+        $slidesContainer = $container.find(options.slidesContainerSelector),
+        $slides = $slidesContainer.children(options.slideSelector);
 
-    var SliderModule = (function() {
-      // Module
-      var m = {};
+    var transitionStrategy = new SamSlider.FadeTransition($slidesContainer, $slides);
 
-      m.init = function() {
-        var transitionStrategy = new SamSlider.FadeTransition($slidesContainer, $slides);
-        m.controller =
-          new SamSlider.Controller(transitionStrategy, $slides.length, { circular: options.circular });
+    var controller =
+      new SamSlider.Controller(
+            transitionStrategy,
+            $slides.length,
+            { circular: options.circular });
 
-        m.showItem(0);
-        buildPagination();
-        attachControlsClickHandler();
+    var carouselInterval,
+        showingSlideEventName = "sam-slider:showing-slider",
+        showSlideEventName = "sam-slider:show-slide";
 
-        if(config.auto) { carousel(); }
-
-        return m;
-      };
-
-      m.goToNextSlide = function() {
-        m.controller.showNextNode();
-        if (config.auto) { startCarousel(); }
-      };
-
-      m.goToPreviousSlide = function() {
-        m.controller.showPreviousNode();
-        if (config.auto) { startCarousel(); }
-      };
-
-      m.showItem = function (slideIndex) {
-        m.controller.showNode(slideIndex);
-      };
-
-      var buildPagination = function() {
-        if(!config.pagination) return;
-
-        $paginationContainer.html("<ul></ul>");
-
-        var $pagination = $paginationContainer.find("ul");
-
-        $slides.each( function(i) {
-          $pagination.append("<li><a href=\"#\">"+(i+1)+"</a></li>");
-        });
-
-        $pagination.find("li:eq(0)").addClass("active");
-
-        attachPaginationClickHandler();
-      };
-
-      var attachPaginationClickHandler = function() {
-        $paginationContainer.on("click", "a", function() {
-          var $pageCtn = $(this).parents("li");
-
-          m.showItem($pageCtn.index());
-
-          return false;
-        });
-      };
-
-      var attachControlsClickHandler = function() {
-        $nextButton.on("click", function() {
-          m.goToNextSlide();
-          return false;
-        });
-
-        $previousButton.on("click", function() {
-          m.goToPreviousSlide();
-          return false;
-        });
-      };
-
-      var carousel =  function() {
-        $slides.
-          mouseenter(function(){ clearInterval(m.carouselRotator); }).
-          mouseleave(startCarousel);
-
-        startCarousel();
-      };
-
-      var startCarousel = function() {
-        clearInterval(m.carouselRotator);
-
-        m.carouselRotator = setInterval( function() {
-          m.goToNextSlide();
-        }, config.speed);
-      };
-
-      return m.init();
-    })();
-
-    return {
-      goToNextSlide: function() {
-        SliderModule.goToNextSlide();
-      },
-      goToPreviousSlide: function() {
-        SliderModule.goToPreviousSlide();
-      },
-      goToSlide: function(slideIndex) {
-        SliderModule.showItem(slideIndex);
-      }
+    var stopCarousel = function () {
+      if (!options.auto) { return; }
+      clearInterval(carouselInterval);
     };
+
+    var startCarousel = function () {
+      if (!options.auto) { return; }
+      carouselInterval = setInterval(goToNextSlide, options.speed);
+    };
+
+    var restartCarousel = function () {
+      stopCarousel();
+      startCarousel();
+    };
+
+    var markTheActiveItemInPagination = function (index) {
+      var links = $paginationContainer.find("a");
+
+      links.removeClass("active");
+      links.eq(index).addClass("active");
+    };
+
+    var attachControlsEvents = function () {
+      $nextButton.on("click", function (evt) {
+        evt.preventDefault();
+        goToNextSlide();
+      });
+
+      $previousButton.on("click", function (evt) {
+        evt.preventDefault();
+        goToPreviousSlide();
+      });
+    };
+
+    var buildPagination = function () {
+      if (!options.pagination) { return; }
+
+      $slides.each(function(i) {
+        $paginationContainer.append("<a href=\"javascript:void(0)\">"+ (i + 1) + "</a>");
+      });
+
+      $paginationContainer.on("click", "a", function () {
+        showSlide($(this).index());
+      });
+    };
+
+    var goToNextSlide = function () {
+      controller.showNextNode();
+      $container.trigger(showingSlideEventName, controller.visibleNodeIndex);
+    };
+
+    var goToPreviousSlide = function () {
+      controller.showPreviousNode();
+      $container.trigger(showingSlideEventName, controller.visibleNodeIndex);
+    };
+
+    var showSlide = function (index) {
+      controller.showNode(index);
+      $container.trigger(showingSlideEventName, index);
+    };
+
+    $container.on(showingSlideEventName, function (evt, slideIndex) {
+      markTheActiveItemInPagination(slideIndex);
+      restartCarousel();
+    });
+
+    $container.on(showSlideEventName, function (evt, slideIndex) {
+      showSlide(slideIndex);
+    });
+
+    $slides.mouseenter(stopCarousel).mouseleave(startCarousel);
+
+    attachControlsEvents();
+
+    buildPagination();
+
+    showSlide(0);
+
+    return $container;
   };
 })(jQuery);
